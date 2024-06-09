04341ed8cdf9f5c0b4b2eeae6bf4315ad238150c
FaizMK340 <faizmk340@gmail.com>
2024-06-08 22:31:58 -0700

    NavBar Change

--- lib/views/Home/home.dart
+++ lib/views/Home/home.dart
@@ -1,3 +1,5 @@
+
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:doctor_app/views/Home/home_view.dart';
 import 'package:doctor_app/views/appointment_view/appointment_view.dart';
 import 'package:doctor_app/views/category/category.dart';
@@ -25,42 +27,29 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screenList.elementAt(selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.black87,
-        type: BottomNavigationBarType.fixed,
-       // backgroundColor: Colors.blue,
-        selectedLabelStyle: TextStyle(color: Colors.black),
-        selectedIconTheme: IconThemeData(color: Colors.brown),
-        currentIndex: selectedIndex,
-        onTap: (value) {
-          setState(() {
-            selectedIndex = value;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, size: 28),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.book, size: 28),
-            label: "Appointment",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.location_on, size: 28),
-            label: "Map",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.category, size: 28),
-            label: "Category",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings, size: 28),
-            label: "Setting",
-          ),
-        ],
-      ),
-    );
-  }
+        body: screenList.elementAt(selectedIndex),
+        bottomNavigationBar: CurvedNavigationBar(
+            key: GlobalKey(),
+            index: selectedIndex,
+            items: <Widget>[
+              Icon(Icons.home, size: 30, color: selectedIndex == 0 ? Colors.red : Colors.black87),
+              Icon(Icons.book, size: 30, color: selectedIndex == 1 ? Colors.red : Colors.black87),
+              Icon(Icons.location_on, size: 30, color: selectedIndex == 2 ? Colors.red : Colors.black87),
+              Icon(Icons.category, size: 30, color: selectedIndex == 3 ? Colors.red : Colors.black87),
+              Icon(Icons.settings, size: 30, color: selectedIndex == 4 ? Colors.red : Colors.black87),
+            ],
+            color: Colors.white,
+            buttonBackgroundColor: Colors.white,
+            backgroundColor: Colors.grey.shade400,
+            animationCurve: Curves.decelerate,
+            animationDuration: const Duration(milliseconds: 600),
+            onTap: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            letIndexChange: (index) => true,
+            ),
+       );
+    }
 }
